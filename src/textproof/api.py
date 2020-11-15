96f30ecb6d8cf65030482e0a436d8a105b64b038
Jason C. McDonald <jcmcdonald@mousepawmedia.com>
2020-11-14 17:30:59 -0800

    Fix error in API call

--- src/textproof/api.py
+++ src/textproof/api.py
@@ -2,17 +2,15 @@ import requests
 
 
 def api_query(text):
-    url = "https://languagetool.org/api/v2/check",
     lang = "en-US"
     response = requests.post(
-        url,
+        "https://languagetool.org/api/v2/check",
         headers={"Content-Type": "application/json"},
         data={"text": text, "language": lang},
     )
     if response.status_code == 200:
         software = response.json()["software"]
         print(f"{software['name']} v{software['version']}")
-        print(f"{url}")
         print(f"{response.json()['language']['name']}")
         return response.json()["matches"]
     else:
