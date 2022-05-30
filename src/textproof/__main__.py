f81472a12840796c9505f6e41ace9902d94700d7
Jason C. McDonald <codemouse92@outlook.com>
2022-05-30 15:28:06 -0500

    Update __main__.py

--- src/textproof/__main__.py
+++ src/textproof/__main__.py
@@ -8,10 +8,6 @@ from textproof.checked_text import CheckedText
 @click.argument('path')
 @click.option('--output', default=None, help="the path to write to")
 def main(path, output):
-    import cProfile, pstats
-    pr = cProfile.Profile()
-    pr.enable()
-
     file = FileIO(path, output)
     try:
         file.load()
@@ -25,19 +21,25 @@ def main(path, output):
 
     file.save()
 
-    pr.disable()
-    stats = pstats.Stats(pr)
-    stats.strip_dirs()
-    stats.sort_stats(pstats.SortKey.CUMULATIVE)
-    stats.print_stats(10)
+    # import cProfile, pstats
+    # with cProfile.Profile() as pr:
+    #     file = FileIO(path, output)
+    #     try:
+    #         file.load()
+    #     except FileNotFoundError:
+    #         print(f"Could not open file {path}")
+    #         return
+
+    #     check = CheckedText(file.data)
+    #     check.fix_typos()
+    #     file.data = str(check)
+
+    #     file.save()
+    # stats = pstats.Stats(pr)
+    # stats.strip_dirs()
+    # stats.sort_stats(pstats.SortKey.CUMULATIVE)
+    # stats.print_stats(10)
 
 
 if __name__ == "__main__":
     main()
-    # import cProfile, pstats
-    # pr = cProfile.Profile()
-    # pr.enable()
-    # main()
-    # pr.disable()
-    # stats = pstats.Stats(pr).sort_stats(pstats.SortKey.CUMULATIVE)
-    # stats.print_stats(10)
